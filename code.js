bc28427e99ba5d1fe4c5f7cd819776e0a0e9ff60
Vaibhav-jain-2008 <76032294+Vaibhav-jain-2008@users.noreply.github.com>
2021-02-15 08:56:23 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,105 @@
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Select an option") {
+    setText("text_area1", "Please select an option");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "How are you") {
+    setText("text_area1", "I am fine");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Hi") {
+    setText("text_area1", "Hi");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Who created you") {
+    setText("text_area1", "Vaibhav jain created me");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "What is your name") {
+    setText("text_area1", "V-Bot");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "What is your mood") {
+    setText("text_area1", "😊😊");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Tell me a joke") {
+    setText("text_area1", "I want t lose weight but weight does not want to lose me");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Toss a coin") {
+    open("https://www.google.com/search?q=flip+a+coin");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Bye") {
+    setText("text_area1", "Bye");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Thanks") {
+    setText("text_area1", "Welcome");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Ask me a riddle") {
+    setText("text_area1", "I go up and come down but I am not elevator answer:Stairs");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "What are you doing") {
+    setText("text_area1", "Chatting with you");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Why were you created") {
+    setText("text_area1", "For chatting with you");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Open youtube") {
+    open("https://www.youtube.com/");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Who is Vaibhav") {
+    setText("text_area1", "Vaibhav is a coder");
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Tell me the time") {
+open ("https://studio.code.org/projects/applab/dZz_ItuzPsaKdLPW5AQvoi6vnpLd-KlQ0_LEUol8EIo")
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Open code.org") {
+open ("https://studio.code.org/home")
+}
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Tell me a proverb") {
+setText("text_area1","Alls well that ends well")
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Open Hotstar") {
+open ("https://www.hotstar.com/in")
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Open Amazon") {
+open ("https://www.amazon.in/")
+  }
+});
+onEvent("button_ANSWER", "click", function( ) {
+  if (getText("text_input1") == "Open Calculator") {
+open ("https://studio.code.org/projects/applab/mIHX4zlB5m1Hr4L23tMlFmVsE0SV52ATNLyX1M_EYfw");
+  }
+});
